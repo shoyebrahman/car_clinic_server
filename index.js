ff0b09d1757759e99ed4bbe7ae8ab38531c141d4
shoyebrahman <imad64236@gmail.com>
2025-11-14 20:00:39 +0600

    crud adding complite

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json()); 
 
 
-console.log(process.env.DB_PASS)
+//console.log(process.env.DB_PASS)
 
 
 
@@ -31,12 +31,82 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection = client.db('car_clinic').collection('services')
+    const checkoutCollection = client.db('car_clinic').collection('checkout')
+
+
+    app.get('/services', async(req, res) =>{
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/services/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query ={ _id: new ObjectId(id)}
+
+       const options = {
+        // Include only the 'title' and 'imdb' fields in the return document
+        projection: { _id: 1, title: 1, price: 1, service_id: 1, img: 1},
+       }
+
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result)
+
+    })
+
+    //checkout
+
+    app.get('/checkout', async(req, res) => {
+      console.log(req.query.email);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email}
+      }
+      
+      const result = await checkoutCollection.find(query).toArray();
+
+      res.send(result)
+    })
+
+
+    app.post('/checkout', async (req, res) => {
+      const checkout = req.body;
+      console.log(checkout)
+      const result = await checkoutCollection.insertOne(checkout);
+      res.send(result);
+    })
+
+    app.delete('/checkout/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result = await checkoutCollection.deleteOne(query)
+      res.send(result);
+    })
+
+    app.patch('/checkout/:id', async(req, res) =>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updatedCheckout = req.body;
+      console.log(updatedCheckout)
+      const updateDoc = {
+        $set: {
+          status: updatedCheckout.status
+        },
+      };
+      const result = await checkoutCollection.updateOne(filter, updateDoc);
+      res.send(result)
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
