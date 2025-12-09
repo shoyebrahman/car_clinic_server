6e6fe1487932f9bc18e5f6eb98f1cf939aa98812
shoyebrahman <imad64236@gmail.com>
2025-12-09 19:11:12 +0600

    deploy vercel complite

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 
 //MIDDLEWARE
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://car-clinic-275af.web.app',
+    'https://car-clinic-275af.firebaseapp.com'
+    ],
+  credentials: true
+}));
 app.use(express.json()); 
+app.use(cookieParser());
 
 
 //console.log(process.env.DB_PASS)
@@ -27,16 +37,74 @@ const client = new MongoClient(uri, {
   }
 });
 
+//middleware
+const logger = async(req, res, next) =>{
+  console.log('called', req.host, req.originalUrl)
+  next();
+}
+
+const verifyToken = async(req, res, next) =>{
+  const token = req.cookies?.token;
+  console.log('value of token in middleware', token)
+  if(!token){
+    return res.status(401).send({message: 'not authorize'})
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        //error
+        if(err){
+          console.log(err);
+          return res.status(401).send({message: 'unauthorize'})
+        }
+        //if token is valid then it would be decoded
+        console.log('value in the token', decoded)
+        req.user = decoded;
+        next()
+      })
+}
+
+
+ const cookieOptions = {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  partitioned: true,       
+  maxAge: 24 * 60 * 60 * 1000
+};
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    //await client.connect();
 
     const serviceCollection = client.db('car_clinic').collection('services')
     const checkoutCollection = client.db('car_clinic').collection('checkout')
 
+    //auth related api
+    app.post('/jwt', logger, async(req, res) =>{
+      const user = req.body;
+      console.log(user)
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      res
+       .cookie('token', token, 
+           cookieOptions,
+        
+       )
+      .send({success: true})
+      //res.send(user)
+    })
 
-    app.get('/services', async(req, res) =>{
+    // jwt for logout
+
+    app.post('/logout', async(req, res) => {
+      const user = req.body;
+      console.log('logging out', user);
+      res.clearCookie('token', { maxAge: 0}).send({ success: true})
+    })
+
+
+   //service related api
+    app.get('/services', logger, async(req, res) =>{
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
       res.send(result);
@@ -58,12 +126,19 @@ async function run() {
 
     //checkout
 
-    app.get('/checkout', async(req, res) => {
+    app.get('/checkout', logger, verifyToken, async(req, res) => {
       console.log(req.query.email);
+      console.log('user in the valid token', req.user)
+      //console.log('tok tok token', req.cookies.token)
+      console.log('user in the valid token', req.user)
+      if(req.query.email !== req.user.email) {
+        return res.status(401).send({message:'forbiden access'})
+      }
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email}
       }
+      
       
       const result = await checkoutCollection.find(query).toArray();
 
@@ -102,7 +177,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    //await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
